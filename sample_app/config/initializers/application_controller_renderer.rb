78dd96249c556acfb0edc4ce552add45e19b0646
Masaki <masaki4200@Masaki4200.local>
2020-01-11 18:31:01 +0900

    [Finish] 2-9

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ceaefa4f3e2db30dc41349f82992b3cc63ff2a5b869b85935e16e787cc2760abe3eb3686d4e30b4207f8f5dca96a26335f4da0a2e75326a7052d66c31d88d97f'
